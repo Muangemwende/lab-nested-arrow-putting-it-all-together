3c7f7f93fa2e27630b21056dc0375bbc1de56e57
janette <janet.muange@student.moringa.com>
2026-04-06 22:22:58 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-
-
-
+function createLoginTracker(userInfo){
+  let attemptCount = 0;
+  const handleLogin = (passwordAttempt) => {
+    attemptCount ++;
+    if(passwordAttempt === userInfo.password){
+      return `log in successfully ${attemptCount}`;
+    }
+    else{
+      return`wrong password ${attemptCount}`;
+    }
+  };
+  return handleLogin;
+};
+const user = {
+  username: "user1",
+  password: "password123"
+};
+const login = createLoginTracker(user);
+console.log(login("password123"));
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
